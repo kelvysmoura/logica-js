535b5e5e909c7aff663e9d16aff44bd6fb3d7234
kelvysmoura <kelvysmoura2@gmail.com>
2023-12-29 16:52:50 -0300

    Adicionando mais casos de teste para o desafio de somar dosi numeros

--- .github/desafio/soma.provider.js
+++ .github/desafio/soma.provider.js
@@ -19,6 +19,30 @@ module.exports = [
     expected: 12
   },
   {
+    params: [5.3, 1.8],
+    expected: 7.1
+  },
+  {
+    params: ["1.4", 9.7],
+    expected: 11.1
+  },
+  {
+    params: ["1.4", "9.7"],
+    expected: 11.1
+  },
+  {
+    params: [-1, -5],
+    expected: -6
+  },
+  {
+    params: [-5, 5],
+    expected: 0
+  },
+  {
+    params: [3, -5],
+    expected: -2
+  },
+  {
     params: [3],
     expected: error
   },
