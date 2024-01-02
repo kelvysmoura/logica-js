18fb77fb622cb37f3f1aca77ce327a727db18a58
kelvysmoura <kelvysmoura2@gmail.com>
2024-01-02 18:34:24 -0300

    Corrigindo chamada do help markdown no setup.js

--- .github/desafio/setup.js
+++ .github/desafio/setup.js
@@ -1,12 +1,12 @@
 
 
-const md = require('./md-msg');
+const {mdmsg} = require('./help');
 
 const currentBranch = process.env.GITHUB_HEAD_REF;
 const validBranches = process.env.KMP_BRACHES_MERGE_MASTER.split(',')
 
 if (!validBranches.includes(currentBranch)) {
-  md.error(`Branch **${currentBranch}** não é válida.\nBraches válidas: ${validBranches.join(', ')}`)
+  mdmsg.error(`Branch **${currentBranch}** não é válida.\nBraches válidas: ${validBranches.join(', ')}`)
 }
 
 const currentFile = process.env.KMP_FILE_EXECUTE
@@ -16,14 +16,14 @@ try {
   const func = require(currentFile)
 
   if (typeof func !== 'function') {
-    md.error(`Não foi possível encontrar a função \`${currentBranch}\` no arquivo src/${currentBranch}.js`)
+    mdmsg.error(`Não foi possível encontrar a função \`${currentBranch}\` no arquivo src/${currentBranch}.js`)
   }
 
 } catch(error) {
 
   if (error.code === 'MODULE_NOT_FOUND') {
-    md.error(`Não foi possível carregar o arquivo src/${currentBranch}.js`)
+    mdmsg.error(`Não foi possível carregar o arquivo src/${currentBranch}.js`)
   }
 
-  md.error(error.message)
+  mdmsg.error(error.message)
 }
