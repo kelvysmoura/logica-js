8959ec62b9a7b48e8df9c16b993a2f8d699d12f6
kelvysmoura <kelvysmoura2@gmail.com>
2024-01-02 14:36:43 -0300

    Criando js que adiciona regras na branch master

---
+++ .github/setting-respository/branch-protection.js
@@ -0,0 +1,27 @@
+
+const api = require("./api")
+
+
+const uri = 'branches/master/protection';
+const method = 'PUT';
+
+(async () => {
+  try { 
+
+    const payload = require('./payload/branch-protection');
+
+    const request = await api.branchProtection(payload)
+    const statusCode = parseInt(request.status);
+
+    const json = await request.json();
+
+    if (statusCode < 200 || statusCode > 299) {
+      throw new Error(JSON.stringify(json));
+    }
+
+    console.log(body)
+
+  } catch (error) {
+    console.log("- error: " + error.message);
+  }
+})();
---
+++ .github/setting-respository/payload/branch-protection.js
@@ -0,0 +1,11 @@
+
+module.exports = { 
+	"lock_branch": false, 
+	"enforce_admins": true, 
+	"restrictions": null, 
+	"required_status_checks": { 
+		"strict": true, 
+		"contexts": ["Verificando requisitos..."] 
+	}, 
+	"required_pull_request_reviews": null 
+}
