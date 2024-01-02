559549e33205c72c6de95e8ee6bed16290c6beb6
kelvysmoura <kelvysmoura2@gmail.com>
2024-01-02 14:30:36 -0300

    Criando help para requisições api

---
+++ .github/setting-respository/api.js
@@ -0,0 +1,27 @@
+
+const repository = process.env.GITHUB_REPOSITORY;
+const token = process.env.GITHUB_TOKEN;
+const url = 'https://api.github.com';
+
+module.exports = {
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${token}`
+  },
+
+  branchProtection(body) {
+    return fetch(`${url}/repos/${repository}/branches/master/protection`, {
+      method: 'PUT',
+      headers,
+      body: JSON.stringify(body)
+    });
+  },
+
+  createIssue(body) {
+    return fetch(`${url}/repos/${repository}/issues`, {
+      method: "POST",
+      headers,
+      body: JSON.stringify(body)
+    });
+  }
+}
