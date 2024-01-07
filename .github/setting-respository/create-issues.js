89bfc54b6f1e684cffb06b81e1f8cb25d7207d91
kelvysmoura <kelvysmoura2@gmail.com>
2024-01-07 00:25:57 -0300

    Tantativa de burlar o rate-limit do github api

--- .github/setting-respository/create-issues.js
+++ .github/setting-respository/create-issues.js
@@ -21,7 +21,7 @@ const api = require('./api');
 
       console.log(json)
 
-      await (new Promise(resolve => setTimeout(resolve, 2000)))
+      await (new Promise(resolve => setTimeout(resolve, 5000)))
     }
 
   } catch (error) {
