f8d3df7d546750ac77665d4495852cfe7edefe38
kelvysmoura <kelvysmoura2@gmail.com>
2024-01-08 14:09:26 -0300

    Removendo debug do create iassues

--- .github/setting-respository/create-issues.js
+++ .github/setting-respository/create-issues.js
@@ -9,20 +9,19 @@ const api = require('./api');
 
     for (let item of issuesPayload) {
 
-      let st = setTimeout(async () => {
-        const request = await api.createIssue(item)
+      const request = await api.createIssue(item)
 
-        const statusCode = parseInt(request.status);
+      const statusCode = parseInt(request.status);
 
-        const json = await request.json();
-        json.HEADERS = Array.from(request.headers)
+      const json = await request.json();
+      // json.HEADERS = Array.from(request.headers)
 
-        if (statusCode < 200 || statusCode > 299) {
-          throw new Error(JSON.stringify(json));
-        }
+      if (statusCode < 200 || statusCode > 299) {
+        throw new Error(JSON.stringify(json));
+      }
 
-        console.log(json)
-      }, 5000)
+      console.log(json)
+      
 
     }
 
